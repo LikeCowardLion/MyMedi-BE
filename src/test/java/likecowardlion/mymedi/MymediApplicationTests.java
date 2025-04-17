84b77969502406e34621bbbdc9804dfda3851c5b
niireymik <155354743+niireymik@users.noreply.github.com>
2025-04-17 13:11:12 +0900

    Initial commit

---
+++ src/test/java/likecowardlion/mymedi/MymediApplicationTests.java
@@ -0,0 +1,13 @@
+package likecowardlion.mymedi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymediApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
